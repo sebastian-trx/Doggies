d2b53e323625288a4cf1424a73f63143ec7c6227
sebastian-trx <torressebastian014@gmail.com>
2021-09-26 23:20:22 -0500

    get a /dogs y query

--- api/src/prueba.js
+++ api/src/prueba.js
@@ -4,12 +4,21 @@ async function getUser() {
     try {
       const response = await axios.get('https://api.thedogapi.com/v1/breeds');
       //console.log(response.data)
-      let a = 0
-      response.data.map(el => a+=1)
-      console.log(a)
+      const arr = response.data.map(el => {
+        return{
+          name: el.name,
+          height: el.height.metric,
+          weight: el.weight.metric,
+          life_span: el.life_span,
+          image: Object.values(el.image),
+          temperament: el.temperament
+        }
+      })
+      console.log(arr)
     } catch (error) {
       console.error(error);
     }
   }
 
 getUser()
+
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,6 @@
 const { Router } = require('express');
+const axios = require('axios');
+const {Dog,Temperament} = require('../db');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -7,6 +9,54 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+const getApiInfo = async () => {
+    try {
+        const apiUrl = await axios.get('https://api.thedogapi.com/v1/breeds')
+        const apiInfo = await apiUrl.data.map(el => {
+            return{
+                name: el.name,
+                height: el.height.metric,
+                weight: el.weight.metric,
+                life_span: el.life_span,
+                image: Object.values(el.image),
+                temperament: el.temperament
+            }
+        })
+        return apiInfo;
+    } catch (error) {
+        console.log(error)
+    }
+}
 
+const getDbInfo = async () => {
+    return await Dog.findAll({
+        include:{
+            model: Temperament,
+            attributes: ['name'],
+            through:{
+                attributes: []
+            }
+        }
+    })
+}
+
+const getAllDogs = async () => {
+    let apiInfo = await getApiInfo()
+    let DbInfo = await getDbInfo()
+    let All = apiInfo.concat(DbInfo)
+    return All
+}
+
+router.get('/dogs', async(req,res) =>{
+    let {raza} = req.query
+    let all = await getAllDogs()
+    if(raza){
+        let dog = await all.filter(el => el.name.toLowerCase().includes(raza.toLowerCase()))
+        dog.length ?
+        res.status(200).send(dog):
+        res.send('Ups no existe esa raza')
+    }
+    res.status(200).send(all)
+})
 
 module.exports = router;
