79ff8462ba26331460ad7fec2f9cf0e6a430b465
sebastian-trx <torressebastian014@gmail.com>
2021-10-12 20:52:11 -0500

    botones paginado

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -69,7 +69,6 @@ export function Home() {
         FilterByBreed={<FilterByBreed />}
         FilterByTemperaments={<FilterByTemperaments />}
       />
-
       <Cards currentDogs={currentDogs} />
     </div>
   );
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,21 +1,22 @@
-import styles from './styles/Home.module.css'
+import styles from "./styles/Home.module.css";
 export function Sidebar({
-    BreedFromApiOrUser,
-    FilterAscDesc,
-    MasMenosPesado,
-    FilterByBreed,
-    FilterByTemperaments,
-  }) {
-    return(
-        <div className={styles.Sidebar}>
-            filtar por:
-            <div className = {styles.Sidebar_1}>
-            <p>{FilterByBreed}</p>
-            <p>{BreedFromApiOrUser}</p>
-            <p>{FilterByTemperaments}</p>
-            <p>{MasMenosPesado}</p>
-            <p>{FilterAscDesc}</p>
-            </div>
-        </div>
-    )
+  BreedFromApiOrUser,
+  FilterAscDesc,
+  MasMenosPesado,
+  FilterByBreed,
+  FilterByTemperaments,
+}) {
+  return (
+    <div className={styles.Sidebar}>
+      <div className={styles.Sidebar_1}>
+        <strong>Filtar por:</strong>
+        <p>{FilterByBreed}</p>
+        <p>{BreedFromApiOrUser}</p>
+        <strong>Ordenar por:</strong>
+        <p>{FilterByTemperaments}</p>
+        <p>{MasMenosPesado}</p>
+        <p>{FilterAscDesc}</p>
+      </div>
+    </div>
+  );
 }
