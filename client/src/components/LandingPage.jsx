b6552a5c09dd447d3de71c89e23dc44d7e9dff80
Sebastián Torres <torressebastian014@gmail.com>
2022-01-30 12:11:09 -0500

    remove warning style

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import styles from "./styles/LandingPage.module.css";
+// import styles from "./styles/LandingPage.module.css";
 import s from "./styles/LandingPage2.module.css";
 
 export function LandingPage(params) {
